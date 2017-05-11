8cab07f7badba362ae17cda7919ffc1a4fc8d409
ricavalcanti <ricjose2005@hotmail.com>
2017-05-11 18:41:21 -0300

    Atualização classe nutricionista
    Novos atributos para nutricionistas

--- Domain /edu/ufrn/pdsoft/model/Nutricionista.groovy
+++ Domain /edu/ufrn/pdsoft/model/Nutricionista.groovy
@@ -1,6 +1,14 @@
 package edu.ufrn.pdsoft.model
 
 class Nutricionista {
+	String name
+	String localCrn
+	String nomeLogin
+	String senhaLogin
+	double crn
+	double cpf
+	double rg
+
 
     static constraints = {
     }
