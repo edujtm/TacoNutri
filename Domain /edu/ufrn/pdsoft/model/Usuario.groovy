e903276ce09d4e8d019582134d865ec7a4f4a7a3
ricavalcanti <ricjose2005@hotmail.com>
2017-05-11 18:58:23 -0300

    Classe usuario criada

---
+++ Domain /edu/ufrn/pdsoft/model/Usuario.groovy
@@ -0,0 +1,12 @@
+package edu.ufrn.pdsoft.model
+
+class Usuario {  
+	String nome
+	String nomeLogin
+	String senhaLogin
+	int cpf
+	int rg
+	//adicionar classe da dieta
+    static constraints = {
+    }
+}
